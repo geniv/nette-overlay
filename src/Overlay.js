16cd3bc8af2669254b531e78a8b1f95195e848bd
Martin[cze] Fugess <martin.fugess@gmail.com>
2018-02-13 20:14:32 +0100

    upravy pro realne pouziti

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,33 +1,33 @@
 $(document).ready(function () {
-    $(".nette-overlay-open").on("click", function (event) {
+    $(".nette-overlay__open").on("click", function (event) {
         event.preventDefault();
 
         var specificNetteOverlay = $(this).data("specific");
-        $("body").addClass("nette-overlay-on");
-        $(".nette-overlay[data-specific='" + specificNetteOverlay + "']").addClass("on");
-        $(".nette-overlay.out[data-specific='" + specificNetteOverlay + "']").removeClass("out end");
+        $("body").addClass("nette-overlay--shown");
+        $(".nette-overlay[data-specific='" + specificNetteOverlay + "']").addClass("nette-overlay--on");
+        $(".nette-overlay.out[data-specific='" + specificNetteOverlay + "']").removeClass("nette-overlay--out nette-overlay--end");
 
         if ($(".nette-overlay[data-specific='" + specificNetteOverlay + "'] form")[0]) {
             $(".nette-overlay[data-specific='" + specificNetteOverlay + "'] form").find("input[type='text']").first().focus();
         }
     });
 
-    $(".nette-overlay-close, .nette-overlay-backdrop").on("click", function (event) {
+    $(".nette-overlay__close, .nette-overlay__backdrop").on("click", function (event) {
         event.preventDefault();
-        $(".nette-overlay.on").addClass("out").removeClass("on");
-        $("body").removeClass("nette-overlay-on");
+        $(".nette-overlay.nette-overlay--on").addClass("nette-overlay--out").removeClass("nette-overlay--on");
+        $("body").removeClass("nette-overlay--shown");
         $(".nette-overlay").one("webkitAnimationEnd oanimationend msAnimationEnd animationend", function () {
-            $(".nette-overlay.out").addClass("end");
+            $(".nette-overlay.nette-overlay--out").addClass("nette-overlay--end");
         });
     });
 });
 
 $(document).keyup(function (e) {
     if (e.keyCode == 27) {
-        $(".nette-overlay.on").addClass("out").removeClass("on");
-        $("body").removeClass("nette-overlay-on");
+        $(".nette-overlay.nette-overlay--on").addClass("nette-overlay--out").removeClass("nette-overlay--on");
+        $("body").removeClass("nette-overlay--shown");
         $(".nette-overlay").one("webkitAnimationEnd oanimationend msAnimationEnd animationend", function () {
-            $(".nette-overlay.out").addClass("end");
+            $(".nette-overlay.nette-overlay--out").addClass("nette-overlay--end");
         });
     }
 });
